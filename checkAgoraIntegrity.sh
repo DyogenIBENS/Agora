a14a92464a4fc0df576d3ad52edda64ed3661718
Matthieu Muffato <muffato@ebi.ac.uk>
2020-05-02 15:42:39 +0100

    Simplified the script by factoring out print functions

--- checkAgoraIntegrity.sh
+++ checkAgoraIntegrity.sh
@@ -7,28 +7,36 @@ red='\e[0;31m'
 green='\e[0;32m'
 NC='\e[0m' # No Color
 
+print_and_run_commands () {
+    for line in "$@"
+    do
+        printf "${green}${line}${NC}\n"
+        eval "$line"
+    done
+}
+
+# from https://stackoverflow.com/questions/5349718/how-can-i-repeat-a-character-in-bash
+print_title () {
+    t=$1
+    l=$(eval "printf '%0.s=' {1..${#t}}")
+    printf "${red}${l}${NC}\n"
+    printf "${red}${t}${NC}\n"
+    printf "${red}${l}${NC}\n"
+}
+
+error () {
+    printf "${red}HOHOHO problem! $1${NC}\n"
+    exit 1
+}
+
 #creates tmp/ directory
-printf "${red}---------------------------------${NC}\n"
-printf "${red}creates tmp directory for testing${NC}\n"
-printf "${red}---------------------------------${NC}\n"
-if [ -d tmp ]
-    then
-        printf "${green}mkdir tmp${NC}\n"
-        rm -r tmp
-        mkdir tmp
-    else
-        printf "${green}mkdir tmp${NC}\n"
-        mkdir tmp
-fi
-
-
+print_title 'creates tmp directory for testing'
+print_and_run_commands "rm -rf tmp" "mkdir tmp"
 
 #############################################
 #	Check integrity of pre-processing scripts #
 #############################################
-printf "${red}-------------------------------${NC}\n"
-printf "${red}check the preprocessing scripts${NC}\n"
-printf "${red}-------------------------------${NC}\n"
+print_title 'check the preprocessing scripts'
 
 preProcessCommandLines=(
 # convet a .nhx tree into a protTree (forest of gene trees)
@@ -36,72 +44,51 @@ preProcessCommandLines=(
 # convet a .nwk tree into a phylTree
 "src/preprocessing/newickSpeciesTree2phylTreeSpeciesTree.py example/data/Species.nwk > tmp/speciesTree.phylTree"
 )
-for line in "${preProcessCommandLines[@]}"
-	do
-		printf "${green}${line}${NC}\n"
-		eval ${line}
-done
+print_and_run_commands "${preProcessCommandLines[@]}"
 
 ###################################################
 #	Check integrity of ALL.extractGeneFamilies.py #
 ###################################################
-printf "${red}----------------------------------------------${NC}\n"
-printf "${red}check the ancGenes families extraction scripts${NC}\n"
-printf "${red}----------------------------------------------${NC}\n"
+print_title 'check the ancGenes families extraction scripts'
 
 extractGeneFamiliesCommandLines=(
 "src/ALL.extractGeneFamilies.py tmp/speciesTree.phylTree tmp/geneTrees.protTree -OUT.ancGenesFiles=tmp/ancGenes/all/ancGenes.%s.list.bz2 > tmp/geneTrees.afterExtractingAncGenes.protTree"
 )
-for line in "${extractGeneFamiliesCommandLines[@]}"
-	do
-		printf "${green}${line}${NC}\n"
-		eval ${line}
-done
+print_and_run_commands "${extractGeneFamiliesCommandLines[@]}"
 
 #########################################
 #	Check integrity of agora.py		    #
 #########################################
-printf "${red}----------------------------------${NC}\n"
-printf "${red}creation of the configuration file${NC}\n"
-printf "${red}----------------------------------${NC}\n"
+print_title 'creation of the configuration file'
 
 sed  s,../example/data/Species.conf,speciesTree.phylTree, ./conf/agora-robust.ini > tmp/agora-robust.ini
 
-printf "${red}--------------------------------------${NC}\n"
-printf "${red}check the agora.py encapsulated script${NC}\n"
-printf "${red}--------------------------------------${NC}\n"
+print_title 'check the agora.py encapsulated script'
 agoraCommandLines=(
 # agora.py
 "src/agora.py tmp/agora-robust.ini -workingDir=tmp"
 )
 
-for line in "${agoraCommandLines[@]}"
-	do
-		printf "${green}${line}${NC}\n"
-		eval ${line}
-done
+print_and_run_commands "${agoraCommandLines[@]}"
 
 NbAncDiags=`ls tmp/integrDiags/final/diags.* | wc -l`
 
 if [ ${NbAncDiags} != 4 ]
     then
-        printf "${red} HOHOHO problems! ${NC}\n"
-        exit 1
+        error 'Missing ancestors'
 fi
 
 #########################################
 #	Check integrity of postprocessing   #
 #########################################
 
-printf "${red}--------------------------------${NC}\n"
-printf "${red}check the postprocessing script ${NC}\n"
-printf "${red}--------------------------------${NC}\n"
+print_title 'check the postprocessing script'
 
 mkdir tmp/ancGenomes
 convertAncGenomesCommandLines=(
 "src/postprocessing/misc.convertContigsToGenome.py tmp/speciesTree.phylTree A0 -IN.ancDiags=tmp/integrDiags/final/diags.%s.list.bz2 -OUT.ancGenomes=tmp/ancGenomes/ancGenome.%s.list.bz2 -ancGenesFiles=tmp/ancGenes/all/ancGenes.%s.list.bz2"
 )
-eval "${convertAncGenomesCommandLines[@]}"
+print_and_run_commands "${convertAncGenomesCommandLines[@]}"
 
 NbAncGenomes=`ls tmp/ancGenomes/ancGenome.* | wc -l`
 
@@ -110,7 +97,6 @@ if [ ${NbAncGenomes} == 4 ]
         printf "${red} the ancestral genomes are available in tmp/ancGenomes/${NC}\n"
         printf "${red} Everything seems OK! Enjoy AGORA${NC}\n"
     else
-        printf "${red} HOHOHO problems! ${NC}\n"
-        exit 1
+        error 'Some ancestral genomes are missing'
 fi
 
