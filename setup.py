437185df50e78bbda2baa37e80bdd6ddbba47c08
Franklin Delehelle <delehelle@ens.fr>
2023-02-24 09:48:41 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='Agora',
-      version='1.2.3',
+      version='1.3.0',
       description='Ancestral Genome Reconstruction',
       author='DYOGEN lab',
       author_email='agora@bio.ens.psl.eu',
